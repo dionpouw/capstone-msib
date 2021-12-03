79f2c31d4416d0add52c0f8cfa467ba7576beaaa
mjafarshidik <mjafarshidik86@gmail.com>
2021-12-03 20:43:35 +0700

    Change user class to data class

--- capstonemsib/app/src/main/java/com/aldion/capstonemsib/ui/signin/User.kt
+++ capstonemsib/app/src/main/java/com/aldion/capstonemsib/ui/signin/User.kt
@@ -1,11 +1,11 @@
 package com.aldion.capstonemsib.ui.signin
 
-class User {
-    var email: String? = ""
-    var name: String? = ""
-    var password: String? = ""
-    var dateOfBirth: String? = ""
-    var url: String? = ""
-    var username: String? = ""
+data class User (
+    var email: String? = "",
+    var name: String? = "",
+    var password: String? = "",
+    var dateOfBirth: String? = "",
+    var url: String? = "",
+    var username: String? = "",
     var telephoneNumber: String? = ""
-}
+)
