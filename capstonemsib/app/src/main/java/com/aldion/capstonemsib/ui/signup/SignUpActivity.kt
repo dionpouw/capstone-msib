0b7756081f12e0d161df55c25a3fd75afc463f84
Aldion <aldion.sumampouw@gmail.com>
2021-12-03 09:41:41 +0800

    Revert "Merge pull request #21 from mjafarshidik/jafar_progress"
    This reverts commit 4a92cb0a7b73e97a12d062549acc6c5fbdad728d.

--- capstonemsib/app/src/main/java/com/aldion/capstonemsib/ui/signup/SignUpActivity.kt
+++ capstonemsib/app/src/main/java/com/aldion/capstonemsib/ui/signup/SignUpActivity.kt
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aldion.capstonemsib.databinding.ActivitySignUpBinding
 import com.aldion.capstonemsib.ui.signin.SignInActivity
-import com.aldion.capstonemsib.data.entity.User
-import com.google.firebase.database.*
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var signUpBinding: ActivitySignUpBinding
@@ -26,43 +24,5 @@ class SignUpActivity : AppCompatActivity() {
                 startActivity(intentSignUp)
             }
         }
-    }
-
-    private fun saveUsername(
-        suUsername: String,
-        suPassword: String,
-        suName: String,
-        suEmail: String
-    ) {
-        val user = User()
-        user.email = suEmail
-        user.username = suUsername
-        user.name = suName
-        user.password = suPassword
-
-        checkingUsername(suUsername, user)
-    }
-
-    private fun checkingUsername(suUsername: String, data: User) {
-        mDatabaseReference.child(suUsername).addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val user = snapshot.getValue(User::class.java)
-                if (user == null) {
-                    mDatabaseReference.child(suUsername).setValue(data)
-
-                    val intentSignIn = Intent(this@SignUpActivity, SignInActivity::class.java)
-                    startActivity(intentSignIn)
-
-                } else {
-                    Toast.makeText(this@SignUpActivity, "Akun kamu berhasil didaftarkan", Toast.LENGTH_LONG)
-                        .show()
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(this@SignUpActivity, "" + error.message, Toast.LENGTH_LONG).show()
-            }
-
-        })
     }
 }
