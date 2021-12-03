ae455f989fd17694c3ea7e089cf3995852198eb5
mjafarshidik <mjafarshidik86@gmail.com>
2021-12-03 08:17:06 +0700

    Update class User

--- capstonemsib/app/src/main/java/com/aldion/capstonemsib/ui/signin/User.kt
+++ capstonemsib/app/src/main/java/com/aldion/capstonemsib/ui/signin/User.kt
@@ -1,11 +1,11 @@
 package com.aldion.capstonemsib.ui.signin
 
 class User {
-    var email: String ? = ""
-    var name: String ? = ""
-    var password: String ? = ""
-    var dateOfBirth: String ? = ""
-    var url: String ? = ""
-    var username : String ? = ""
-    var telephoneNumber : String ? = ""
+    var email: String? = ""
+    var name: String? = ""
+    var password: String? = ""
+    var dateOfBirth: String? = ""
+    var url: String? = ""
+    var username: String? = ""
+    var telephoneNumber: String? = ""
 }
