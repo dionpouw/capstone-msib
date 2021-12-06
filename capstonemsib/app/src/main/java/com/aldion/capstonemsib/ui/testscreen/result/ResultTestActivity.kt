3f4439496ee2e5ac83a42378ff85251efa56426a
mjafarshidik <mjafarshidik86@gmail.com>
2021-12-06 19:54:08 +0700

    Update test fragment layout

--- capstonemsib/app/src/main/java/com/aldion/capstonemsib/ui/testscreen/result/ResultTestActivity.kt
+++ capstonemsib/app/src/main/java/com/aldion/capstonemsib/ui/testscreen/result/ResultTestActivity.kt
@@ -6,6 +6,7 @@ import com.aldion.capstonemsib.databinding.ActivityResultTestBinding
 
 class ResultTestActivity : AppCompatActivity() {
     private var binding: ActivityResultTestBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityResultTestBinding.inflate(layoutInflater)
