e0ce104a00523e17527dc6d9caf8468012b3d93f
mjafarshidik <mjafarshidik86@gmail.com>
2021-12-07 20:50:36 +0700

    Home fragment initialization

--- capstonemsib/app/src/main/java/com/aldion/capstonemsib/ui/home/HomeFragment.kt
+++ capstonemsib/app/src/main/java/com/aldion/capstonemsib/ui/home/HomeFragment.kt
@@ -1,16 +1,28 @@
 package com.aldion.capstonemsib.ui.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.aldion.capstonemsib.databinding.FragmentHomeBinding
+import com.aldion.capstonemsib.ui.testscreen.TestFragment
+import com.aldion.capstonemsib.utils.Preferences
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding
+
+    private lateinit var preferences: Preferences
+    private lateinit var mDatabase : DatabaseReference
+    //private var datalist = ArrayList<>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,8 +35,23 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding?.button?.setOnClickListener {
-            
+        preferences = Preferences(requireActivity().applicationContext)
+        mDatabase = FirebaseDatabase.getInstance().getReference("Psychology")
+
+        binding?.apply {
+            tvName.text = preferences.getValue("name")
+
+            Glide.with(this@HomeFragment)
+                .load(preferences.getValue("url"))
+                .apply(RequestOptions.circleCropTransform())
+                .into(ivProfile)
+
+            Log.v("test", "url "+preferences.getValues("url"))
+
+            itemHomeTest.iRight.setOnClickListener(){
+                val intent = Intent(activity, TestFragment::class.java)
+                startActivity(intent)
+            }
         }
     }
 
