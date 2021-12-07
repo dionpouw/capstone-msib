e94d862d715372d2a8d2b3d32e99019c3347651f
mjafarshidik <mjafarshidik86@gmail.com>
2021-12-07 11:53:54 +0700

    Added entity statement and StatementDao

---
+++ capstonemsib/app/src/main/java/com/aldion/capstonemsib/data/room/StatementDao.kt
@@ -0,0 +1,16 @@
+package com.aldion.capstonemsib.data.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.aldion.capstonemsib.data.Statement
+
+@Dao
+interface StatementDao {
+    @Query("SELECT * FROM statement_table")
+    fun getAllStatement(): LiveData<MutableList<Statement?>?>?
+
+    @Insert
+    fun insert(statement: Statement?)
+}
