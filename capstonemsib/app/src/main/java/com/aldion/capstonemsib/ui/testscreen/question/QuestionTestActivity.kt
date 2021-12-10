e57547aa1fc081c76273c35b66a46c830aefe3b1
Aldion <aldion.sumampouw@gmail.com>
2021-12-10 12:41:01 +0800

    logic added for test

--- capstonemsib/app/src/main/java/com/aldion/capstonemsib/ui/testscreen/question/QuestionTestActivity.kt
+++ capstonemsib/app/src/main/java/com/aldion/capstonemsib/ui/testscreen/question/QuestionTestActivity.kt
@@ -1,12 +1,19 @@
 package com.aldion.capstonemsib.ui.testscreen.question
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import com.aldion.capstonemsib.R
 import com.aldion.capstonemsib.data.entity.Statement
 import com.aldion.capstonemsib.databinding.ActivityQuestionTestBinding
+import com.aldion.capstonemsib.ui.testscreen.result.ResultTestActivity
 import com.aldion.capstonemsib.utils.ViewModelFactory
 
 class QuestionTestActivity : AppCompatActivity(), View.OnClickListener {
@@ -31,10 +38,10 @@ class QuestionTestActivity : AppCompatActivity(), View.OnClickListener {
 
         setQuestion()
 
-        binding?.btnOption1?.setOnClickListener(this)
-        binding?.btnOption2?.setOnClickListener(this)
-        binding?.btnOption3?.setOnClickListener(this)
-        binding?.btnOption4?.setOnClickListener(this)
+        binding?.tvOption1?.setOnClickListener(this)
+        binding?.tvOption2?.setOnClickListener(this)
+        binding?.tvOption3?.setOnClickListener(this)
+        binding?.tvOption4?.setOnClickListener(this)
         binding?.btnNext?.setOnClickListener(this)
         binding?.btnPrevious?.setOnClickListener(this)
 
@@ -50,16 +57,69 @@ class QuestionTestActivity : AppCompatActivity(), View.OnClickListener {
         } else {
             binding?.btnNext?.text = "Submit"
         }
-        binding?.pbQuestion?.progress= mCurrentPosition
+        binding?.pbQuestion?.progress = mCurrentPosition
         binding?.tvProgress?.text = "$mCurrentPosition" + "/" + binding?.pbQuestion?.max
-        binding?.btnOption1?.text = question.optA
-        binding?.btnOption2?.text = question.optB
-        binding?.btnOption3?.text = question.optC
-        binding?.btnOption4?.text = question.optD
+        binding?.tvOption1?.text = question.optA
+        binding?.tvOption2?.text = question.optB
+        binding?.tvOption3?.text = question.optC
+        binding?.tvOption4?.text = question.optD
 
     }
 
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView?>()
+        options.add(0, binding?.tvOption1)
+        options.add(1, binding?.tvOption2)
+        options.add(2, binding?.tvOption3)
+        options.add(3, binding?.tvOption4)
+
+        for (option in options) {
+            option?.typeface = Typeface.DEFAULT
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.setTextColor(Color.parseColor("#FFFFFF"))
+        tv.background = ContextCompat.getDrawable(this, R.drawable.shape_rectangle_primary)
+    }
+
     override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+        when (v?.id) {
+            R.id.tvOption1 -> {
+                binding?.tvOption1?.let { selectedOptionView(it, selectedOptionNum = 1) }
+            }
+            R.id.tvOption2 -> {
+                binding?.tvOption2?.let { selectedOptionView(it, selectedOptionNum = 2) }
+            }
+            R.id.tvOption3 -> {
+                binding?.tvOption3?.let { selectedOptionView(it, selectedOptionNum = 3) }
+            }
+            R.id.tvOption4 -> {
+                binding?.tvOption4?.let { selectedOptionView(it, selectedOptionNum = 4) }
+            }
+            R.id.btnNext -> {
+                if (mSelectedOptionPosition == 0) {
+                    when {
+                        mCurrentPosition <= mQuestionList!!.size -> {
+                            setQuestion()
+                        }
+                        else -> {
+                            val intent = Intent(this, ResultTestActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+                } else {
+                    if (mCurrentPosition == mQuestionList!!.size) {
+                        binding?.btnNext?.text = getString(R.string.finish)
+                    } else {
+                        binding?.btnNext?.text = getString(R.string.next_question)
+                    }
+                    mSelectedOptionPosition = 0
+                }
+            }
+        }
     }
 }
