c1889526d1dcd17b36f3e45c2e96290bf9732451
mjafarshidik <mjafarshidik86@gmail.com>
2021-12-18 18:55:49 +0700

    Implement Log Out

--- capstonemsib/app/src/main/java/com/aldion/capstonemsib/ui/profile/ProfileEditFragment.kt
+++ capstonemsib/app/src/main/java/com/aldion/capstonemsib/ui/profile/ProfileEditFragment.kt
@@ -7,6 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.aldion.capstonemsib.databinding.FragmentProfileEditBinding
 import com.aldion.capstonemsib.utils.Preferences
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.database.DataSnapshot
 
 class ProfileEditFragment : Fragment() {
     private var _binding: FragmentProfileEditBinding? = null
@@ -28,7 +31,17 @@ class ProfileEditFragment : Fragment() {
         preferences = Preferences(requireContext())
 
         binding?.apply {
+//            edtNameProfile.text = preferences.child("name")
+//            edtEmailProfile.text = preferences.getValue("email")
+//            edtDateProfile.text = preferences.getValue("dateOfBirth")
+//            edtTelephoneProfile.text = preferences.getValue("telephoneNumber")
+//            edtUsernameProfile.text = preferences.getValue("username")
+//            edtPasswordProfile.text = preferences.getValue("password").toString()
 
+            Glide.with(this@ProfileEditFragment)
+                .load(preferences.getValue("url"))
+                .apply(RequestOptions.circleCropTransform())
+                .into(imgProfile)
         }
     }
 
